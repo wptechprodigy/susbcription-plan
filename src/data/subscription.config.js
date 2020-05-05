ba13731764326f0b7801d41b2a6e147f9bdf990d
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-05 14:57:53 +0100

    separate data

---
+++ src/data/subscription.config.js
@@ -0,0 +1,34 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const subscriptionPlans = [
+	{
+		id: uuidv4(),
+		name: 'Bronze',
+		color: 'bg-pink-400',
+		description:
+			'Ideal for one-person email teams who want to design and build beautiful emails that look great in every inbox.',
+		price: 45.99,
+		features: ['Builder', 'Instant email reviews', 'Checklist', 'Page testing'],
+	},
+	{
+		id: uuidv4(),
+		name: 'Silver',
+		color: 'bg-indigo-400',
+		description:
+			'Ideal for email teams of 2-3 people who want to automate testing and improve campaign effectiveness.',
+		price: 65.99,
+		features: ['Builder', 'Instant email reviews', 'Checklist', 'Page testing'],
+	},
+	{
+		id: uuidv4(),
+		name: 'Gold',
+		color: 'bg-teal-400',
+		description:
+			'Ideal for agencies and email teams of 4+ people who want to boost collaboration and drive the best results at scale.',
+		price: 95.99,
+		features: ['Builder', 'Instant email reviews', 'Checklist', 'Page testing'],
+	},
+];
+
+
+export default subscriptionPlans;
