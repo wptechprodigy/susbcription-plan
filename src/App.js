0e932a987c9a2fba8e47289694657c843354991c
wptechprodigy <waheed.freelancer16@gmail.com>
2020-05-05 15:40:26 +0100

    modify styles

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Plans from './components/plans';
 
 function App() {
 	return (
-		<section className='flex flex-col justify-center align-middle m-20'>
+		<section className='flex flex-col justify-center align-middle m-10'>
 			<h1 className='flex justify-center text-center align-middle text-4xl font-bold'>
 				Our Subscription Plans
 			</h1>
-			<span className='flex col-gap-5 justify-center align-middle pt-20'>
+			<span className='flex flex-col row-gap-5 md:col-gap-5 md:flex-row justify-center align-middle pt-20'>
 				{subscriptionPlans.map((subscriptionPlan) => (
-					<Plans key={subscriptionPlan.id} subscriptionPlan={subscriptionPlan} />
+					<Plans
+						key={subscriptionPlan.id}
+						subscriptionPlan={subscriptionPlan}
+					/>
 				))}
 			</span>
 		</section>
--- src/components/plans/Plan.js
+++ src/components/plans/Plan.js
@@ -6,18 +6,24 @@ function Plan({ subscriptionPlan }) {
 
 	return (
 		<>
-			<div className={`flex flex-col justify-center align-middle ${color} w-full pt-10 pb-4 pl-10 pr-10`}>
+			<div
+				className={`flex flex-col justify-center align-middle ${color} w-full pt-10 pb-4 pl-10 pr-10 overflow-hidden rounded`}>
 				<p className='text-center font-semibold text-2xl'>{name}</p>
-				<h3 className='font-bold text-center text-4xl'>${price}</h3>
-				<p className='text-center font-medium text-white my-5'>
-					{description}
-				</p>
+				<h3 className='font-bold text-center'>
+					<span className='inline-block align-top pt-2 text-2xl font-semibold'>
+						$
+					</span>
+					<span className='text-4xl'>{price}</span>
+				</h3>
+				<p className='text-center font-medium text-white my-5'>{description}</p>
 				<button className='bg-green-700 hover:bg-white hover:text-green-700 text-white font-bold py-2 pb-3 px-4 rounded'>
 					Sign up
 				</button>
 			</div>
 			<div className='w-full text-center p-5'>
-				{features.map(feature => <Feature key={feature} feature={feature} />)}
+				{features.map((feature) => (
+					<Feature key={feature} feature={feature} />
+				))}
 			</div>
 		</>
 	);
